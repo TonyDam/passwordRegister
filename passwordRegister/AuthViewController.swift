e519330430b70dbd7b9dde1749c619e266a574a1
Tony Dam <tony>
2020-04-30 11:03:57 +0200

    Dans le Storyboard, création et personnalisation de la vue Auth View Controller

--- passwordRegister/AuthViewController.swift
+++ passwordRegister/AuthViewController.swift
@@ -14,6 +14,7 @@ class AuthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupUI()
     }
     
     
