52b85e0456ff0d62edc4ec421a911094cfeefeab
Tony Dam <tony>
2020-04-30 11:45:59 +0200

    control + drag pour les outlets + override viewWillAppear + validateBtnDidPressed (control + drag + action) dans le fichier authviewcontroller

--- passwordRegister/AuthViewController.swift
+++ passwordRegister/AuthViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 // C'est ici qu'on voit si un user est conecter ou pas, s'il est connecter il fera un segue vers le controller suivant
 class AuthViewController: UIViewController {
-
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var nicknameTF: UITextField!
+    @IBOutlet weak var validateBtn: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupUI()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setupUI()
+    }
+    
+    @IBAction func validateBtnDidPressed(_ sender: Any) {
     }
     
     
