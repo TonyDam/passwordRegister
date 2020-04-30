755cab1b8ac7fd2aa6ce4184228d754a5cc0c1a3
Tony Dam <tony>
2020-04-30 10:50:56 +0200

    Création de la fonction setupUI() dans AuthViewController.swift, permettant de modifier l'UI si l'user est co ou non, si oui on lance le segue de la 2eme page

--- passwordRegister/AuthViewController.swift
+++ passwordRegister/AuthViewController.swift
@@ -15,7 +15,18 @@ class AuthViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    // Fonction qui change l'UI selon si l'user est co ou pas
+    func setupUI() {
+        if FireAuth().isAuth() {
+            // s'il est authentifié, on naviguera vers la 2eme page
+            print("Utilisateur authentifiée")
+        } else {
+            // Sinon l'user doit s'authentifier
+            print("Utilisateur non authentifié")
+        }
+    }
 
 }
 
